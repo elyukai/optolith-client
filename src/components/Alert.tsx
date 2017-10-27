1cf1ef8fd0f3e5a815d20dc52ed181df8577b74b
Elytherion <lukas.obermann@live.de>
2017-10-27 18:32:40 +0200

    Fixed alerts onClose and ArmorZones undo

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -12,60 +12,68 @@ export interface AlertProps {
 	dispatch(action: Action): void;
 }
 
-export class Alert extends React.Component<AlertProps> {
-	render() {
-		const { close, dispatch, options, locale } = this.props;
-		let buttons: ViewAlertButton[] | undefined;
-		let message;
-		let title;
+export function Alert(props: AlertProps) {
+	const { close, dispatch, options, locale } = props;
+	let buttons: ViewAlertButton[] | undefined;
+	let message;
+	let title;
+	let onClose: (() => void) | undefined;
 
-		if (options) {
-			const {
-				buttons: buttonsOption = [{ label: 'OK', autoWidth: true }],
-				message: messageOption,
-				title: titleOption,
-				confirm,
-				confirmYesNo
-			} = options;
+	if (options) {
+		const {
+			buttons: buttonsOption = [{ label: 'OK', autoWidth: true }],
+			message: messageOption,
+			title: titleOption,
+			confirm,
+			confirmYesNo,
+			onClose: onCloseOption
+		} = options;
 
-			buttons = (confirm ? [
-				{
-					label: confirmYesNo ? _translate(locale, 'yes') : _translate(locale, 'ok'),
-					dispatchOnClick: confirm[0]
-				},
-				{
-					label: confirmYesNo ? _translate(locale, 'no') : _translate(locale, 'cancel'),
-					dispatchOnClick: confirm[1]
+		buttons = (confirm ? [
+			{
+				label: confirmYesNo ? _translate(locale, 'yes') : _translate(locale, 'ok'),
+				dispatchOnClick: confirm[0]
+			},
+			{
+				label: confirmYesNo ? _translate(locale, 'no') : _translate(locale, 'cancel'),
+				dispatchOnClick: confirm[1]
+			}
+		] : buttonsOption).map(e => {
+			const { dispatchOnClick, ...other } = e;
+			return { ...other, onClick: () => {
+				if (dispatchOnClick) {
+					dispatch(dispatchOnClick);
 				}
-			] : buttonsOption).map(e => {
-				const { dispatchOnClick, ...other } = e;
-				return { ...other, onClick: () => {
-					if (dispatchOnClick) {
-						dispatch(dispatchOnClick);
-					}
-				}};
-			});
-			message = messageOption;
-			title = titleOption;
-		}
-
-		if (buttons && buttons.length === 1) {
-			remote.globalShortcut.register('Enter', () => {
-				remote.globalShortcut.unregister('Enter');
-				close();
-			});
-		}
-
-		return (
-			<Dialog
-				close={close}
-				buttons={buttons}
-				isOpened={options !== null}
-				className="alert"
-				title={title}
-				>
-				{message}
-			</Dialog>
-		);
+			}};
+		});
+		message = messageOption;
+		title = titleOption;
+		onClose = onCloseOption;
 	}
+
+	const closeEnhanced = () => {
+		if (onClose) {
+			onClose();
+		}
+		close();
+	};
+
+	if (buttons && buttons.length === 1) {
+		remote.globalShortcut.register('Enter', () => {
+			remote.globalShortcut.unregister('Enter');
+			closeEnhanced();
+		});
+	}
+
+	return (
+		<Dialog
+			close={closeEnhanced}
+			buttons={buttons}
+			isOpened={options !== null}
+			className="alert"
+			title={title}
+			>
+			{message}
+		</Dialog>
+	);
 }
