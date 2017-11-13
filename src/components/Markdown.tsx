82d3a003166c2dae4c9ba86fb68208b470feed88
Elytherion <lukas.obermann@live.de>
2017-11-13 23:32:11 +0100

    Finally fixed Markdown

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -7,9 +7,24 @@ export interface MarkdownProps {
 	source: string;
 }
 
+export interface MarkdownRootProps {
+	children?: React.ReactNode;
+}
+
 export function Markdown(props: MarkdownProps) {
-	const { className = '', source = '...', isListElement } = props;
+	const { className, source = '...', isListElement } = props;
+	const root = (props: MarkdownRootProps) => isListElement ? (
+		<ul className={className}>{props.children}</ul>
+	) : (
+		<div className={className}>{props.children}</div>
+	);
 	return (
-		<ReactMarkdown source={source} skipHtml className={className} renderers={{ root: isListElement ? <ul/> : <div/> }} />
+		<ReactMarkdown
+			source={source}
+			skipHtml
+			renderers={{
+				root
+			}}
+			/>
 	);
 }
