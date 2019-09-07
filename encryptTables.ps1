87f16408450f232c8d711af2c24609c33b55ef17
Lukas Obermann <lukas.obermann@outlook.de>
2019-09-07 22:41:05 +0200

    Updated tables and script to encrypt tables

--- encryptTables.ps1
+++ encryptTables.ps1
@@ -7,18 +7,25 @@ if($insider) {
   $source = "..\..\OneDrive\TDE app\data\insider"
 }
 
-$filter = [regex] "TDE5(?:_[a-z]{2}-[A-Z]{2})\.xlsx"
+Write-Output "Source: $(Resolve-Path -Path $source)"
+
+$filter = [regex] "^TDE5(?:_[a-z]{2}-[A-Z]{2})\.xlsx$"
 
 if (!(Test-Path $target -PathType Container)) {
   New-Item -Path $target -ItemType Directory
 }
 
-Get-ChildItem -Path $target -Include *.* -File | foreach { $_.Delete()}
+Write-Output "Target: $(Resolve-Path -Path $target)"
+
+Get-ChildItem -Path $target -Include *.* -File | ForEach-Object { $_.Delete() }
 
 $allTables = Get-ChildItem -Path $source | Where-Object {$_.Name -match $filter}
 
 foreach ($item in $allTables) {
   Copy-Item -Path $item.FullName -Destination $target
+  Write-Output "Copied $($item.FullName) to $(Resolve-Path -Path $target)"
 }
 
 python .\encryptTables.py $target
+
+Write-Output "Table encryption succeeded!"
