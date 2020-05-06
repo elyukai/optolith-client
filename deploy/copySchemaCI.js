9bdf3560b527c49c6f0d4eed3537c6ea807c12dd
Lukas Obermann <lukas.obermann@outlook.de>
2020-05-06 20:25:39 +0200

    Reverted last commit

--- deploy/copySchemaCI.js
+++ deploy/copySchemaCI.js
@@ -2,4 +2,3 @@ const { copySchema } = require ("./getStaticData")
 const { join } = require ("path")
 
 copySchema (join ("app", "Database", "Schema"), join ("src", "App", "Utilities", "YAML", "Schema"))
-copySchema (join ("app", "Database", "Schema"), join ("app", "Database", "Schema"))
