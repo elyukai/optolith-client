9afd9c6e1b35b1c286577575e8d1a7baaf6df27e
Lukas Obermann <lukas.obermann@outlook.de>
2020-05-06 20:21:02 +0200

    FIxed CI script

--- deploy/copySchemaCI.js
+++ deploy/copySchemaCI.js
@@ -2,3 +2,4 @@ const { copySchema } = require ("./getStaticData")
 const { join } = require ("path")
 
 copySchema (join ("app", "Database", "Schema"), join ("src", "App", "Utilities", "YAML", "Schema"))
+copySchema (join ("app", "Database", "Schema"), join ("app", "Database", "Schema"))
