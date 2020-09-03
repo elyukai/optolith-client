0b3deba653572fb36c76a8fe71555bdda70d34a9
Lukas Obermann <lukas.obermann@outlook.de>
2020-09-04 01:53:18 +0200

    Finished functions to handle skill check attributes

--- src/App/Utilities/Increasable/AttributeSkillCheckMinimum.ts
+++ src/App/Utilities/Increasable/AttributeSkillCheckMinimum.ts
@@ -1,12 +1,28 @@
+/**
+ * This module includes function for working with attribute minima based on
+ * the skill check attributes of active skills, spells, magical actions and
+ * chants.
+ *
+ * To take care of the minimum, at hero initialization a cache must be build and
+ * it must changed if spells, magical actions or chants are added or removed to
+ * keep track of all skill check attributes.
+ *
+ * To actually check for a minimum, you need the cache and multiple parts of the
+ * hero and from those then derive the minimum for a specific attribute based on
+ * the skill checks and skill ratings of all active skills.
+ */
+
 import { flip } from "../../../Data/Function"
 import { cons, consF, foldr, lengthAtLeast, List, nub, sdelete } from "../../../Data/List"
-import { listToMaybe, mapMaybe, maybe, Nothing } from "../../../Data/Maybe"
-import { lookupF, OrderedMap } from "../../../Data/OrderedMap"
+import { ensure, fromJust, isJust, listToMaybe, mapMaybe, maybe, Maybe, Nothing } from "../../../Data/Maybe"
+import { gt, subtractBy } from "../../../Data/Num"
+import { lookup, lookupF, OrderedMap } from "../../../Data/OrderedMap"
 import { Record } from "../../../Data/Record"
 import { fst, Pair, snd } from "../../../Data/Tuple"
 import { curryN5, curryN6 } from "../../../Data/Tuple/All"
 import { ActivatableSkillDependent } from "../../Models/ActiveEntries/ActivatableSkillDependent"
 import { AttributeDependent } from "../../Models/ActiveEntries/AttributeDependent"
+import { SkillDependent } from "../../Models/ActiveEntries/SkillDependent"
 import { LiturgicalChant } from "../../Models/Wiki/LiturgicalChant"
 import { Skill } from "../../Models/Wiki/Skill"
 import { Spell } from "../../Models/Wiki/Spell"
@@ -143,18 +159,152 @@ export const getSingleMaximumAttribute =
       maxAttrIds => lengthAtLeast (2) (maxAttrIds) ? Nothing : listToMaybe (maxAttrIds)
     )
 
-// export const getSkillCheckAttributeMinimum = (
-//   staticSkills: OrderedMap<string, Record<Skill>>,
-//   staticSpells: OrderedMap<string, Record<Spell>>,
-//   staticLiturgicalChants: OrderedMap<string, Record<LiturgicalChant>>,
-//   heroAttributes: OrderedMap<string, Record<AttributeDependent>>,
-//   heroSpells: OrderedMap<string, Record<ActivatableSkillDependent>>,
-//   heroLiturgicalChants: OrderedMap<string, Record<ActivatableSkillDependent>>,
-//   cache: AttributeSkillCheckMinimumCache,
-//   attributeId: string,
-// ) =>
-//   pipe_ (
-//     attributeId,
-//     lookupF (cache),
-//     fmap (skillIds => List.foldr (x => x, 0, skillIds))
-//   )
+const getGenericNewMinimumSr = <A, B> (
+  getCheck: (x: A) => List<string>,
+  getSkillRating: (x: B) => number,
+  staticEntries: OrderedMap<string, A>,
+  heroAttributes: OrderedMap<string, Record<AttributeDependent>>,
+  heroEntries: OrderedMap<string, B>,
+  attributeId: string,
+  currentMinimumSr: number,
+  entryId: string,
+): number => {
+  const maybeStaticEntry = lookup (entryId) (staticEntries)
+  const maybeHeroEntry = lookup (entryId) (heroEntries)
+
+  // A static entry must be present and a hero entry as well, since it has to
+  // have an SR higher than 0 to be relevant
+  if (isJust (maybeStaticEntry)
+      && isJust (maybeHeroEntry)) {
+    const check = pipe_ (maybeStaticEntry, fromJust, getCheck)
+
+    const skillRating = pipe_ (maybeHeroEntry, fromJust, getSkillRating)
+
+    // It's SR not only needs to be higher than 0, but that the current maximum
+    // SR relevant detected to save some calculation work
+    if (skillRating > currentMinimumSr) {
+      const maybeSingleMaximumAttribute =
+        getSingleMaximumAttribute (heroAttributes) (check)
+
+      // There must be only one maximum attribute...
+      if (isJust (maybeSingleMaximumAttribute)) {
+        const singleMaximumAttribute =
+          fromJust (maybeSingleMaximumAttribute)
+
+        // ...and this attribute must be the attribute that is currently checked
+        // for a minimum value.
+        if (singleMaximumAttribute === attributeId) {
+          return skillRating
+        }
+      }
+    }
+  }
+
+  return currentMinimumSr
+}
+
+const getNewMinimumSR = (
+  staticSkills: OrderedMap<string, Record<Skill>>,
+  staticSpells: OrderedMap<string, Record<Spell>>,
+  staticLiturgicalChants: OrderedMap<string, Record<LiturgicalChant>>,
+  heroAttributes: OrderedMap<string, Record<AttributeDependent>>,
+  heroSkills: OrderedMap<string, Record<SkillDependent>>,
+  heroSpells: OrderedMap<string, Record<ActivatableSkillDependent>>,
+  heroLiturgicalChants: OrderedMap<string, Record<ActivatableSkillDependent>>,
+  attributeId: string,
+) => (skillId: V<Type, string>) => (accMinimumSR: number) => pipe_ (
+  skillId,
+  ({ tag, value }) => {
+    switch (tag) {
+      case "Skill": {
+        return getGenericNewMinimumSr (
+          Skill.A.check,
+          SkillDependent.A.value,
+          staticSkills,
+          heroAttributes,
+          heroSkills,
+          attributeId,
+          accMinimumSR,
+          value
+        )
+      }
+
+      case "Spell": {
+        return getGenericNewMinimumSr (
+          Spell.A.check,
+          ActivatableSkillDependent.A.value,
+          staticSpells,
+          heroAttributes,
+          heroSpells,
+          attributeId,
+          accMinimumSR,
+          value
+        )
+      }
+
+      case "LiturgicalChant": {
+        return getGenericNewMinimumSr (
+          LiturgicalChant.A.check,
+          ActivatableSkillDependent.A.value,
+          staticLiturgicalChants,
+          heroAttributes,
+          heroLiturgicalChants,
+          attributeId,
+          accMinimumSR,
+          value
+        )
+      }
+
+      default:
+        return accMinimumSR
+    }
+  }
+)
+
+/**
+ * Returns the attribute minimum based on the created skill check attribute
+ * cache `cache` for the attribute with id `attributeId`.
+ *
+ * Since skills (skills, spells and chants) have a maximum possible skill rating
+ * of "Highest skill check attribute + 2", in turn, the attributes must ensure
+ * they keep themselves on a "Highest skill with this attribute as the highest
+ * skill check attribute - 2".
+ *
+ * This minimum should be added to the list of minima to calculate a total
+ * minimum for the respective attribute.
+ */
+export const getSkillCheckAttributeMinimum = (
+  staticSkills: OrderedMap<string, Record<Skill>>,
+  staticSpells: OrderedMap<string, Record<Spell>>,
+  staticLiturgicalChants: OrderedMap<string, Record<LiturgicalChant>>,
+  heroAttributes: OrderedMap<string, Record<AttributeDependent>>,
+  heroSkills: OrderedMap<string, Record<SkillDependent>>,
+  // TODO: Once this is converted to Reason, dont forget to add all relevant
+  // magical actions here!
+  heroSpells: OrderedMap<string, Record<ActivatableSkillDependent>>,
+  heroLiturgicalChants: OrderedMap<string, Record<ActivatableSkillDependent>>,
+  cache: AttributeSkillCheckMinimumCache,
+  attributeId: string,
+): Maybe<number> =>
+  pipe_ (
+    attributeId,
+    lookupF (cache),
+    maybe (0)
+          (List.foldr (getNewMinimumSR (
+                        staticSkills,
+                        staticSpells,
+                        staticLiturgicalChants,
+                        heroAttributes,
+                        heroSkills,
+                        heroSpells,
+                        heroLiturgicalChants,
+                        attributeId,
+                      ))
+                      (0)),
+    // Highest attribute + 2 is SR maximum, so the attribute minimum is
+    // highest SR - 2
+    subtractBy (2),
+    // clean up the result to only return a number if it's higher than the
+    // default minimum of 8
+    ensure (gt (8))
+  )
