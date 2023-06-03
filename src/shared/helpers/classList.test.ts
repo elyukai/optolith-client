39486a3f2901957a865627a1841514f73d06eae4
Lukas Obermann <lukas.obermann@icloud.com>
2023-06-03 12:04:35 +0200

    test: switch from jest to native node test runner

---
+++ src/shared/helpers/classList.test.ts
@@ -0,0 +1,9 @@
+import assert from "node:assert/strict"
+import { describe, it } from "node:test"
+import { classList } from "./classList.ts"
+
+describe("classList", () => {
+  it("returns a string of class names from the given arguments", () => {
+    assert.equal(classList("foo", "bar", undefined, null, { baz: true, no: false }), "foo bar baz")
+  })
+})
