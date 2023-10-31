0f3d2805b0613e33a04c74fdeeea6036403461ba
Lukas Obermann <lukas.obermann@icloud.com>
2023-10-31 14:40:36 +0100

    feat: add dependency handling to liturgical chants and ceremonies

--- src/main_window/slices/ceremoniesSlice.ts
+++ src/main_window/slices/ceremoniesSlice.ts
@@ -1,6 +1,9 @@
 import { ImprovementCost, fromRaw } from "../../shared/domain/adventurePoints/improvementCost.ts"
+import { registerOrUnregisterPrerequisitesOfLiturgyAsDependencies } from "../../shared/domain/dependencies/fullPrerequisiteRegistrationAsDependencyForType.ts"
+import { createIdentifierObject } from "../../shared/domain/identifier.ts"
 import { createActivatableRatedWithEnhancementsSlice } from "./activatableRatedWithEnhancementsSlice.ts"
 
+// eslint-disable-next-line jsdoc/require-jsdoc
 export const {
   create: createDynamicCeremony,
   createInitial: createInitialDynamicCeremony,
@@ -20,4 +23,8 @@ export const {
     fromRaw(database.ceremonies[id]?.improvement_cost) ?? ImprovementCost.D,
   getEnhancementAdventurePointsModifier: (id, enhancementId, database) =>
     database.ceremonies[id]?.enhancements?.[enhancementId]?.adventure_points_modifier ?? 0,
+  getPrerequisites: (id, database) => database.ceremonies[id]?.prerequisites ?? [],
+  createIdentifierObject: id => createIdentifierObject("Ceremony", id),
+  registerOrUnregisterPrerequisitesAsDependencies:
+    registerOrUnregisterPrerequisitesOfLiturgyAsDependencies,
 })
--- src/main_window/slices/liturgicalChantsSlice.ts
+++ src/main_window/slices/liturgicalChantsSlice.ts
@@ -1,6 +1,9 @@
 import { ImprovementCost, fromRaw } from "../../shared/domain/adventurePoints/improvementCost.ts"
+import { registerOrUnregisterPrerequisitesOfLiturgyAsDependencies } from "../../shared/domain/dependencies/fullPrerequisiteRegistrationAsDependencyForType.ts"
+import { createIdentifierObject } from "../../shared/domain/identifier.ts"
 import { createActivatableRatedWithEnhancementsSlice } from "./activatableRatedWithEnhancementsSlice.ts"
 
+// eslint-disable-next-line jsdoc/require-jsdoc
 export const {
   create: createDynamicLiturgicalChant,
   createInitial: createInitialDynamicLiturgicalChant,
@@ -20,4 +23,8 @@ export const {
     fromRaw(database.liturgicalChants[id]?.improvement_cost) ?? ImprovementCost.D,
   getEnhancementAdventurePointsModifier: (id, enhancementId, database) =>
     database.liturgicalChants[id]?.enhancements?.[enhancementId]?.adventure_points_modifier ?? 0,
+  getPrerequisites: (id, database) => database.liturgicalChants[id]?.prerequisites ?? [],
+  createIdentifierObject: id => createIdentifierObject("LiturgicalChant", id),
+  registerOrUnregisterPrerequisitesAsDependencies:
+    registerOrUnregisterPrerequisitesOfLiturgyAsDependencies,
 })
