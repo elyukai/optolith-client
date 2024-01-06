968a8b62b5dd9787d1f6a1d37db3ca1534254b40
Lukas Obermann <lukas.obermann@icloud.com>
2024-01-06 18:43:05 +0100

    fix: use @ts-expect-error for functions that cannot be implemented yet

--- src/shared/domain/prerequisites/single/animistPowerPrerequisiteValidation.ts
+++ src/shared/domain/prerequisites/single/animistPowerPrerequisiteValidation.ts
@@ -4,6 +4,8 @@ import { AnimistPowerPrerequisite } from "optolith-database-schema/types/prerequ
  * Checks a single animist power prerequisite if it’s matched.
  */
 export const checkAnimistPowerPrerequisite = (
+  // @ts-expect-error missing implementation
   caps: Record<string, never>,
+  // @ts-expect-error missing implementation
   p: AnimistPowerPrerequisite,
 ): boolean => false // TODO: Implementation
--- src/shared/domain/prerequisites/single/influencePrerequisiteValidation.ts
+++ src/shared/domain/prerequisites/single/influencePrerequisiteValidation.ts
@@ -4,6 +4,8 @@ import { InfluencePrerequisite } from "optolith-database-schema/types/prerequisi
  * Checks a single influence prerequisite if it’s matched.
  */
 export const checkInfluencePrerequisite = (
+  // @ts-expect-error missing implementation
   caps: Record<string, never>,
+  // @ts-expect-error missing implementation
   p: InfluencePrerequisite,
 ): boolean => false // TODO: Implementation
